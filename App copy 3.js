28e3a0e42cc53d612c33248cafbce77459cafa7f
Taltal <taltalrealty@gmail.com>
2022-04-22 18:31:01 +0900

    .

---
+++ App copy 3.js
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+export default function App() {
+  await SplashScreen.preventAutoHideAsync();
+}
