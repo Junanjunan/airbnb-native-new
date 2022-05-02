1212830ac2e15a09a3c23f06787ad37af0f5c2c8
Taltal <taltalrealty@gmail.com>
2022-05-02 18:50:02 +0900

    study 5/2 office

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ import { Asset } from 'expo-asset';
 import * as Font from 'expo-font'
 import { Ionicons } from '@expo/vector-icons';
 import Gate from './components/Gate';
+import { Provider } from "react-redux";
+import store, { persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
 
 const cacheImages = images =>
 images.map(image => {
@@ -32,7 +35,11 @@ export default function App() {
     return Promise.all([...fontPromises, ...imagePromises])
   };
   return isReady ? (
-    <Gate />
+    <Provider store={store}>
+      <PersistGate persistor = {persistor}>
+        <Gate />
+      </PersistGate>
+    </Provider>
   ) : (
   <AppLoading 
     onError={console.error} 
